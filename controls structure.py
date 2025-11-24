38eafb25d3a92f3dd8beb5f801ed7bd4b2f95e9d
Ayush Kumar <ayush22kumar37@gmail.com>
2025-11-24 09:44:22 +0530

    control statements if else, if else

---
+++ controls structure.py
@@ -0,0 +1,22 @@
+# if else
+ticket_price = 90
+wallet_size = 100
+
+if ticket_price<=wallet_size:
+    print("i can go for a movie")
+else :
+    print("i can't go to a movie")
+
+
+# if else if 
+marks = 91
+if(marks>=90):
+    print("A+")
+elif(marks>=80):
+    print("A")
+elif(marks>=70):
+    print("B+")
+elif(marks>=60):
+    print("B")
+else:
+    print("Fail")
