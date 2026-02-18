24b62127e709196d8479dd589999452c606a70d7
Ayush Kumar <ayush22kumar37@gmail.com>
2026-02-18 15:18:07 +0530

    basic calculator in python

---
+++ calculator.py
@@ -0,0 +1,45 @@
+# Function for addition
+def add(a, b):
+    return a + b
+
+# Function for subtraction
+def subtract(a, b):
+    return a - b
+
+# Function for multiplication
+def multiply(a, b):
+    return a * b
+
+# Function for division
+def divide(a, b):
+    if b == 0:
+        return "Error! Division by zero is not allowed."
+    return a / b
+
+
+# Main program
+print("Simple Calculator")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print("Result:", add(num1, num2))
+
+elif choice == '2':
+    print("Result:", subtract(num1, num2))
+
+elif choice == '3':
+    print("Result:", multiply(num1, num2))
+
+elif choice == '4':
+    print("Result:", divide(num1, num2))
+
+else:
+    print("Invalid Input")
