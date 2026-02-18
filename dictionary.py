b8ef90537c6d2e45dec676e8dd8ebdfdb1f7e27a
Ayush Kumar <ayush22kumar37@gmail.com>
2026-02-18 16:50:05 +0530

    python dictionaries

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+# dictionary
+# list=[] , dictionary={}
+student={"name":"raju", "age":21, "gender":"male"}
+# these are key-value pair
+# these dictionary accept all data types like string , integer , float etc 
+
+# accessing values
+print(student["name"])
+
+
+#updating values 
+student["name"]="david"
+
+# looping through a dictionary 
+for key in student:
+    print(key)
+
+for k, v in student.items():
+    print(k,v)
+
