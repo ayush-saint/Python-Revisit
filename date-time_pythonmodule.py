7b7cd697ac1a461df8fe33e24e64151c23679897
Ayush Kumar <ayush22kumar37@gmail.com>
2026-02-19 10:28:35 +0530

    date-time python module

---
+++ date-time_pythonmodule.py
@@ -0,0 +1,9 @@
+# date - time module is a kind of python module/library 
+
+import datetime
+from datetime import datetime, date, time, timedelta 
+ 
+curr_datetime = datetime.now()
+print(curr_datetime)
+print(curr_datetime.month)
+print(curr_datetime.year)
